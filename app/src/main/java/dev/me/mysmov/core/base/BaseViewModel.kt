60e1ebca8d5cae92ff7ab81901dd3f10d935ae62
Jeff-Kidzie <jeffry.irmawan.88@gmail.com>
2026-02-05 15:31:41 +0700

    add base view model

--- app/src/main/java/dev/me/mysmov/core/base/BaseViewModel.kt
+++ app/src/main/java/dev/me/mysmov/core/base/BaseViewModel.kt
@@ -8,34 +8,38 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 abstract class BaseViewModel<A : Action, EV : Event, EF : Effect, VS : ViewState> : ViewModel() {
 
-    protected abstract fun initialState() : VS
+    protected abstract fun initialState(): VS
     protected abstract fun handleOnAction(action: A)
-    protected abstract fun reduce(oldState : VS, event : EV) : VS
+    protected abstract fun reduce(oldState: VS, event: EV): VS
 
     // view state flow
     private val _viewState = MutableStateFlow(initialState())
-    val viewState : StateFlow<VS> = _viewState
+    val viewState: StateFlow<VS> = _viewState
 
     private val _effect = MutableSharedFlow<EF>()
-    val effect : SharedFlow<EF> = _effect
+    val effect: SharedFlow<EF> = _effect
+
+    private val eventMutex = Mutex()
 
     fun onAction(action: A) {
         handleOnAction(action)
     }
 
-    protected fun sendEvent(ev : EV) {
-        _viewState.update { old -> reduce(old, ev)}
-    }
-
-    protected fun sendEffect(ef : EF) {
-        viewModelScope.launch {
-            _effect.emit(ef)
+    protected suspend fun sendEvent(ev: EV) {
+        eventMutex.withLock {
+            _viewState.update { old -> reduce(old, ev) }
         }
+
     }
 
+    protected suspend fun sendEffect(ef: EF) {
+        _effect.emit(ef)
+    }
 
 
 }
