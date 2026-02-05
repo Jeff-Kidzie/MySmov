d97eb671e84b49242af02409ed70ff3f1ea7bfa3
Jeff-Kidzie <jeffry.irmawan.88@gmail.com>
2026-02-05 09:50:16 +0700

    update

--- app/src/main/java/dev/me/mysmov/service/ApiService.kt
+++ app/src/main/java/dev/me/mysmov/service/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("/discover/movie")
+    @GET("discover/movie")
     suspend fun getDiscoverMovie(): Response<List<Movie>>
 }
